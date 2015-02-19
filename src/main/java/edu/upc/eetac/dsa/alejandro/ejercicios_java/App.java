511de06433e53131b74c8f025c78813f6cd902ab
Alejandro <Alejandro@10.89.139.176>
2015-02-19 12:21:59 +0100

    ejercicios-java

---
+++ src/main/java/edu/upc/eetac/dsa/alejandro/ejercicios_java/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.alejandro.ejercicios_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola World!" );
+    }
+}
