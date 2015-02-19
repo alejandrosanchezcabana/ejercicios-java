9ea8c744b12bcb39ce62cbcace69f8e27c805deb
Alejandro <Alejandro@10.89.139.176>
2015-02-19 12:41:55 +0100

    Ejercicios java

---
+++ src/main/java/edu/upc/eetac/dsa/alejandro/ex1/Tree.java
@@ -0,0 +1,5 @@
+package edu.upc.eetac.dsa.alejandro.ex1;
+
+public class Tree {
+
+}
---
+++ src/main/java/edu/upc/eetac/dsa/alejandro/ex1/ex1.java
@@ -0,0 +1,11 @@
+package edu.upc.eetac.dsa.alejandro.ex1;
+
+public class App { 
+    public static void main(String args[]) { 
+        Tree[] arboles = new Tree[4];
+        arboles[0] = new Tree(4); 
+        arboles[1] = new Tree("Roble"); 
+        arboles[2] = new Tree(); 
+        arboles[3] = new Tree(5,"Pino");
+    } 
+}
